f1fdedcfc4f4732b8add42927ca10eab7b2c18e2
wind-dust <1002380377@qq.com>
2019-10-15 17:51:34 +0800

    文档及无用接口废除

--- public/doc/api_project.js
+++ public/doc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.0.0",
   "description": "api文档",
   "title": "api接口文档",
-  "url": "http://127.0.0.1:1006",
-  "sampleUrl": "http://127.0.0.1:1006",
+  "url": "http://127.0.0.1:1007",
+  "sampleUrl": "http://127.0.0.1:1007",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-10-15T09:30:36.315Z",
+    "time": "2019-10-15T09:50:11.700Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
