8c57bdd3d6cec285088239140401fd1eb8ec1834
wind-dust <1002380377@qq.com>
2019-10-28 14:51:28 +0800

    调试接口

--- application/common/action/index/Send.php
+++ application/common/action/index/Send.php
@@ -33,7 +33,8 @@ class Send extends CommonIndex {
      * @author zyr
      */
     public function cmppSendTest($mobile, $code) {
-        $this->cmpp->Start("124.251.111.5",9000,"yxyx01","bMtHJY96","","","","");
+        // $this->cmpp->Start("124.251.111.5",9000,"yxyx01","bMtHJY96","","","","");
+        $this->cmpp->Start("47.95.161.134",9004,"280605","lffazqwrke4d","","","","");
         $result = $this->cmpp->sendSms($mobile, $code); //发送短信
         return $result;
         // die;
