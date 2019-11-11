e6ec0619d45961530e266f6ee96ddacc9e1c586c
Wind-dust <1002380377@qq.com>
2019-11-11 20:45:45 +0800

    前置判断方法取消

--- application/index/controller/Send.php
+++ application/index/controller/Send.php
@@ -5,7 +5,7 @@ use app\index\MyController;
 class Send extends MyController {
     protected $beforeActionList = [
         //        'isLogin',//所有方法的前置操作
-        'isLogin' => ['except' => 'cmppSendTest,smsBatch'], //除去getFirstCate其他方法都进行second前置操作
+        // 'isLogin' => ['except' => 'cmppSendTest,smsBatch,getBalanceSmsBatch,getReceiveSmsBatch'], //除去getFirstCate其他方法都进行second前置操作
         //        'three'  => ['only' => 'hello,data'],//只有hello,data方法进行three前置操作
     ];
 
