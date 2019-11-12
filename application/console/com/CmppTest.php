9a0654c81750a9defc37f857928f75c067ae34b7
wind-dust <1002380377@qq.com>
2019-11-12 16:29:21 +0800

    短信发送

--- application/console/com/CmppTest.php
+++ application/console/com/CmppTest.php
@@ -217,9 +217,9 @@ class CmppTest extends Pzlife {
                         // $senddata = explode(":",$send);
 
                         // $mobile = $senddata['mobile_content'];
-                        $mobile = 15201926171;
+                        $mobile = 15599011983;
                         // $code   = $senddata['task_content']; //带签名
-                        $code   = '【优裹徒】您的包裹[中通-733232]已签收，感谢使用'; //带签名
+                        $code   = '【米思米】安全围栏标准组件上市！不用设计，不用外发喷涂，不用组装！低至363.95元，第五天出货！赶紧过来下单吧。https://www.misumi.com.cn/mail/chn-gc19057-ml03/转发无效,详询021-52559388*6197,回T退订。 '; //带签名
                         // $code   = '短信发送测试'; //带签名
                         // print_r($code);die;
                         $code = mb_convert_encoding($code, 'GBK', 'UTF-8');
