3300d7be0dddb7579eae57a2cecf0697dade83ed
wind-dust <1002380377@qq.com>
2019-11-22 20:58:39 +0800

    查询处理包数据

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -241,9 +241,9 @@ class CmppCreateCodeTask extends Pzlife {
         $redisMessageMarketingSend = Config::get('rediskey.message.redisMessageCodeSend');
         // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15738);
         // $send = $this->redis->rPush('index:meassage:marketing:sendtask', 15739);
-        // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15740);
-        // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15741);
-        echo date('Y-m-d H:i:s',time());die;
+        $send = $this->redis->rPush('index:meassage:marketing:sendtask',15740);
+        $send = $this->redis->rPush('index:meassage:marketing:sendtask',15741);
+        // echo date('Y-m-d H:i:s',time());die;
         while (true) {
             $real_length = 1;
             $send        = $this->redis->lpop('index:meassage:marketing:sendtask');
