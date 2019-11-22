5ace9955b979b8f4f9aea0fd2b4ad85cef3a5e95
wind-dust <1002380377@qq.com>
2019-11-22 20:21:56 +0800

    代码更新

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -121,8 +121,8 @@ class CmppCreateCodeTask extends Pzlife {
         return $sendTask[0];
     }
 
-    private function getSendTaskLog($task_no,$mobile) {
-        $getSendTaskSql = "select 'id' from yx_user_send_task_log where delete_time=0 and `task_no` = '".$task_no."' and `mobile` = '".$mobile."'";
+    private function getSendTaskLog($task_no, $mobile) {
+        $getSendTaskSql = "select 'id' from yx_user_send_task_log where delete_time=0 and `task_no` = '" . $task_no . "' and `mobile` = '" . $mobile . "'";
         // print_r($getUserSql);die;
         $sendTask = Db::query($getSendTaskSql);
         if (!$sendTask) {
@@ -131,67 +131,67 @@ class CmppCreateCodeTask extends Pzlife {
         return $sendTask[0];
     }
 
-    public function getNewMessageLog(){
-        $redis                = Phpredis::getConn();
-        $redisMessageCodeSend = 'index:meassage:code:new:deliver:'; //验证码发送任务rediskey
-        $content = 2;
+    public function getNewMessageLog() {
+        $redis                      = Phpredis::getConn();
+        $redisMessageCodeSend       = 'index:meassage:code:new:deliver:'; //验证码发送任务rediskey
+        $content                    = 2;
         $redisMessageCodeSequenceId = 'index:meassage:code:sequence:id:' . $content; //行业通知SequenceId
-        $sequence = $redis->hget($redisMessageCodeSequenceId,2);
-        $sequence = json_decode($sequence,true);
-        $sendTask = $this->getSendTask($sequence['mar_task_id']);
-        $send_log = $this->getSendTaskLog($sendTask['task_no'], $sequence['mobile']);
-        
+        $sequence                   = $redis->hget($redisMessageCodeSequenceId, 2);
+        $sequence                   = json_decode($sequence, true);
+        $sendTask                   = $this->getSendTask($sequence['mar_task_id']);
+        $send_log                   = $this->getSendTaskLog($sendTask['task_no'], $sequence['mobile']);
+
         // $msgid = $body['Msg_Id1'].$body['Msg_Id2'];
         $msgid = 155153131;
         // print_r($send_log);die;
         Db::startTrans();
-                try {
-                    if (empty($send_log)) {
-                        Db::table('yx_user_send_task_log')->insert([
-                            'task_no' => $sendTask['task_no'],
-                            'mobile' => $sequence['mobile'],
-                            'msgid' =>$msgid,
-                            'send_status' =>2,
-                            'create_time' =>time( )
-                            ]);
-            
-                    }else{
-                        Db::table('yx_user_send_task_log')->where('id',$send_log['id'])->update(['msgid' => $msgid]);
-                    }
-                    Db::commit();
-                    $sequence = $redis->hdel($redisMessageCodeSequenceId,2);
+        try {
+            if (empty($send_log)) {
+                Db::table('yx_user_send_task_log')->insert([
+                    'task_no'     => $sendTask['task_no'],
+                    'mobile'      => $sequence['mobile'],
+                    'msgid'       => $msgid,
+                    'send_status' => 2,
+                    'create_time' => time(),
+                ]);
+
+            } else {
+                Db::table('yx_user_send_task_log')->where('id', $send_log['id'])->update(['msgid' => $msgid]);
+            }
+            Db::commit();
+            $sequence = $redis->hdel($redisMessageCodeSequenceId, 2);
         } catch (\Exception $e) {
             exception($e);
             Db::rollback();
-                    
+
         }
-        
+
         for ($i = 0; $i < 5; $i++) {
             $new_redisMessageCodeSend = $redisMessageCodeSend . $i;
             $send                     = $redis->lPop($new_redisMessageCodeSend);
             while ($send) {
                 $newsend = json_decode($send);
-                
+
             }
         }
     }
 
     //免审任务客户
     public function MisumiTaskSend() {
-        $this->redis                = Phpredis::getConn();
+        $this->redis = Phpredis::getConn();
         // $redisMessageMarketingSend = Config::get('rediskey.message.redisMessageCodeSend');
         $redisMessageMarketingSend = Config::get('rediskey.message.redisMessageCodeSend');
-        $send = $this->redis->lpop($redisMessageMarketingSend);
+        $send                      = $this->redis->lpop($redisMessageMarketingSend);
         print_r($send);die;
         do {
             $sendtask = Db::query("SELECT * FROM yx_user_send_task WHERE  `uid` IN (4,6) AND `free_trial` = 1 AND `id` > 3291 LIMIT 1");
             // print_r($sendtask);die;
             $theSend = [];
             if ($sendtask) {
-                $theSend = $sendtask[0];
-                $num = 0;
+                $theSend          = $sendtask[0];
+                $num              = 0;
                 $mobilesend       = explode(',', $theSend['mobile_content']);
-                $send_length     = mb_strlen($theSend['task_content'], 'utf8');
+                $send_length      = mb_strlen($theSend['task_content'], 'utf8');
                 $effective_mobile = [];
                 foreach ($mobilesend as $key => $value) {
                     $num += ceil($send_length / 65) * $theSend['send_num'];
@@ -204,7 +204,7 @@ class CmppCreateCodeTask extends Pzlife {
                     $user_equities = Db::query("SELECT * FROM yx_user_equities WHERE  `uid` =4 AND `business_id` = 5")[0];
                     // print_r($user_equities);die;
                     $channel_id = 2;
-                }elseif($theSend['uid'] == 6) {
+                } elseif ($theSend['uid'] == 6) {
                     $user_equities = Db::query("SELECT * FROM yx_user_equities WHERE  `uid` =6 AND `business_id` = 6")[0];
                     // print_r($user_equities);die;
                     $channel_id = 1;
@@ -213,106 +213,141 @@ class CmppCreateCodeTask extends Pzlife {
                 // print_r($theSend['id']);die;
                 Db::startTrans();
                 try {
-                    
-                    Db::table('yx_user_send_task')->where('id',$theSend['id']) ->update(['free_trial' => 2,'channel_id' =>$channel_id ]);
-                    Db::table('yx_user_equities')->where('id',$user_equities['id']) ->update(['num_balance' => $had_num]);
+
+                    Db::table('yx_user_send_task')->where('id', $theSend['id'])->update(['free_trial' => 2, 'channel_id' => $channel_id]);
+                    Db::table('yx_user_equities')->where('id', $user_equities['id'])->update(['num_balance' => $had_num]);
 
                     Db::commit();
                     $redisMessageMarketingSend = Config::get('rediskey.message.redisMessageCodeSend');
                     foreach ($effective_mobile as $key => $value) {
                         $res = $this->redis->rpush($redisMessageMarketingSend . ":" . $channel_id, $value . ":" . $theSend['id'] . ":" . $theSend['task_content']); //三体营销通道
-                       
+
                         // $this->redis->hset($redisMessageMarketingSend.":2",$value,$id.":".$Content); //三体营销通道
                     }
                 } catch (\Exception $e) {
                     exception($e);
                     Db::rollback();
-                    
+
                 }
             }
         } while ($theSend);
         echo "SUCCESS";
     }
 
-    public function createMessageSendTaskLog(){
-        $this->redis                = Phpredis::getConn();
+    public function createMessageSendTaskLog() {
+        $this->redis = Phpredis::getConn();
+        ini_set('memory_limit', '3072M'); // 临时设置最大内存占用为3G
+        date_default_timezone_set('PRC');
         $redisMessageMarketingSend = Config::get('rediskey.message.redisMessageCodeSend');
         // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15738);
-        
+        $send = $this->redis->rPush('index:meassage:marketing:sendtask', 15739);
+        // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15740);
+        // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15741);
+
         while (true) {
             $real_length = 1;
-            $send = $this->redis->lpop('index:meassage:marketing:sendtask');
+            $send        = $this->redis->lpop('index:meassage:marketing:sendtask');
             if (empty($send)) {
                 exit('taskId_is_null');
             }
-            $sendTask = $this->getSendTask($send);
-            $mobilesend       = explode(',', $sendTask['mobile_content']);
-            $send_length     = mb_strlen($sendTask['task_content'], 'utf8');
+            $sendTask    = $this->getSendTask($send);
+            $mobilesend  = [];
+            $mobilesend  = explode(',', $sendTask['mobile_content']);
+            $send_length = mb_strlen($sendTask['task_content'], 'utf8');
             if ($send_length > 70) {
                 $real_length = ceil($send_length / 67);
             }
             $real_num = 0;
-            $real_num += $real_length* $sendTask['send_num'];
+            $real_num += $real_length * $sendTask['send_num'];
             $channel_id = 0;
             $channel_id = $sendTask['channel_id'];
-            
-            
+            // print_r($channel_id);die;
+
             foreach ($mobilesend as $key => $kvalue) {
-                $prefix = substr($kvalue, 0, 7);
-                if (in_array($channel_id,[2,6,7,8])){
-                    $res    = $this->getNumberSource($prefix);
-                    print_r($res['source'])."\n";
-                    if ($res['source'] == 2) { //米加联通营销
-                        $channel_id = 8; 
-                    } else if ($res['source'] == 1) { //移动
-                        $channel_id = 2; 
-                       
-                    }else if ($res['source' == 3]) {//米加电信营销
-                        $channel_id = 7; 
+                if (in_array($channel_id, [2, 6, 7, 8])) {
+                    // $getSendTaskSql = "select source,province_id,province from yx_number_source where `mobile` = '".$prefix."' LIMIT 1";
+                    $prefix = substr(trim($kvalue), 0, 7);
+
+                    $res = Db::query("SELECT `source`,`province_id`,`province` FROM yx_number_source WHERE `mobile` = '" . $prefix . "' LIMIT 1");
+                    // continue;
+                    $newres = array_shift($res);
+                    // continue;
+                    // exit;
+                    if ($newres) {
+                        if ($newres['source'] == 2) { //米加联通营销
+                            $channel_id = 8;
+                        } else if ($newres['source'] == 1) { //移动
+                            $channel_id = 2;
+
+                        } else if ($newres['source' == 3]) { //米加电信营销
+                            $channel_id = 7;
+                        }
+
+                    } else {
+                        $channel_id = 2;
                     }
+                    // die;
+                    // print_r($newres);
+
+                    // if (!empty($newres)) {
+                    //     if ($newres['source'] == 2) { //米加联通营销
+                    //         $channel_id = 8;
+                    //     } else if ($newres['source'] == 1) { //移动
+                    //         $channel_id = 2;
+
+                    //     }else if ($newres['source' == 3]) {//米加电信营销
+                    //         $channel_id = 7;
+                    //     }
+                    // }else{
+                    //     $channel_id = 2;
+                    // }
                 }
                 $send_log = [];
                 $send_log = [
-                    'task_no' => $sendTask['task_no'],
-                    'uid' => $sendTask['uid'],
-                    'mobile' => $kvalue,
+                    'task_no'     => $sendTask['task_no'],
+                    'uid'         => $sendTask['uid'],
+                    'mobile'      => $kvalue,
                     'send_status' => 2,
                     'create_time' => time(),
                 ];
                 $sendmessage = [
-                    'mobile' => $kvalue, 
-                    'mar_task_id' => $sendTask['id'], 
-                    'content' => $sendTask['task_content'], 
+                    'mobile'      => $kvalue,
+                    'mar_task_id' => $sendTask['id'],
+                    'content'     => $sendTask['task_content'],
                 ];
-                if (Db::query("SELECT id FROM yx_user_send_task_log WHERE `task_no` = '".$sendTask['task_no']."' AND `mobile` = '".$kvalue."' ")) {
+                if (Db::query("SELECT id FROM yx_user_send_task_log WHERE `task_no` = '" . $sendTask['task_no'] . "' AND `mobile` = '" . $kvalue . "' ")) {
                     continue;
                 }
                 Db::startTrans();
-                try {   
+                try {
                     Db::table('yx_user_send_task_log')->insert($send_log);
-                    $res = $this->redis->rpush($redisMessageMarketingSend . ":" . $channel_id,json_encode($sendmessage)); //三体营销通道
+                    $res = $this->redis->rpush($redisMessageMarketingSend . ":" . $channel_id, json_encode($sendmessage)); //三体营销通道
                     Db::commit();
                 } catch (\Exception $e) {
                     Db::rollback();
                 }
             }
             Db::startTrans();
-                try {   
-                    Db::table('yx_user_send_task')->where('id',$sendTask['id'])->update(['real_num'=> $real_num, 'send_status' =>3]);
-                    Db::commit();
-                } catch (\Exception $e) {
-                    Db::rollback();
-                }
+            try {
+                Db::table('yx_user_send_task')->where('id', $sendTask['id'])->update(['real_num' => $real_num, 'send_status' => 3]);
+                Db::commit();
+            } catch (\Exception $e) {
+                Db::rollback();
+            }
         }
     }
 
-    public function getNumberSource($prefix){
-        $getSendTaskSql = "select source,province_id,province from yx_number_source where delete_time=0 and `mobile` = '".$prefix."'";
-        // print_r($getUserSql);die;
-        $sendTask = Db::query($getSendTaskSql);
-        if (!$sendTask) {
-            return [];
+    public function getNumberSource($prefix) {
+        $getSendTaskSql = "select source,province_id,province from yx_number_source where delete_time=0 and `mobile` = '" . $prefix . "'";
+
+        // echo "\n";
+        $NumberSource = Db::query($getSendTaskSql);
+        if (empty($NumberSource)) {
+            return false;
+        } else {
+
+            return $NumberSource[0];
         }
-        return $sendTask[0];
+        // print_r($sendTask);
     }
 }
