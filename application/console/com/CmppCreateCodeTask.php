9be449b6d44acc4b252d21041b48d5302447dd6b
Wind-dust <1002380377@qq.com>
2019-11-23 09:42:57 +0800

    提交内容

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -239,8 +239,8 @@ class CmppCreateCodeTask extends Pzlife {
         ini_set('memory_limit', '3072M'); // 临时设置最大内存占用为3G
         // date_default_timezone_set('PRC');
         $redisMessageMarketingSend = Config::get('rediskey.message.redisMessageCodeSend');
-        $send = $this->redis->rPush('index:meassage:marketing:sendtask',15742);
-        $send = $this->redis->rPush('index:meassage:marketing:sendtask', 15743);
+        // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15742);
+        // $send = $this->redis->rPush('index:meassage:marketing:sendtask', 15743);
         // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15740);
         // $send = $this->redis->rPush('index:meassage:marketing:sendtask',15741);
         // echo date('Y-m-d H:i:s',time());die;
