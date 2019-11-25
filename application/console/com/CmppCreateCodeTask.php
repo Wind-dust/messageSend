87084929b49aaac5f163ef8344adcbe134d20ac2
Wind-dust <1002380377@qq.com>
2019-11-25 20:45:17 +0800

    带马提交

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -398,6 +398,12 @@ class CmppCreateCodeTask extends Pzlife {
         //     } while (!empty($send));
 
         // }
+        echo time()-1574472176;die;
+        for ($i=325790; $i < 375792; $i++) { 
+            $newtime = time()-1574472176;
+            // Db::table('yx_user_send_task_log')->where('id',$i)->update(['create_time' => $send_data['Stat']]);
+        }
+        die;
         $send_status = [
             1 => 20000,
             2 => 40000,
