4774cbc019cb573a8373645d3d044f030bdfac5d
wind-dust <1002380377@qq.com>
2019-11-29 19:02:28 +0800

    字符串转码

--- application/console/com/ServerSocketShuHe.php
+++ application/console/com/ServerSocketShuHe.php
@@ -191,6 +191,8 @@ class ServerSocketShuHe extends Pzlife {
                                $sendData = [];
                                if ($body['Msg_Fmt'] == 15) {
                                    $message = mb_convert_encoding($message, 'UTF-8', 'GBK');
+                               }if ($body['Msg_Fmt'] == 0){
+                                $message = $this->decode($message);
                                }
 
                                $sendData = [
@@ -220,6 +222,8 @@ class ServerSocketShuHe extends Pzlife {
                                $message     = $Msg_Content['Msg_Content'];
                                if ($body['Msg_Fmt'] == 15) {
                                    $message = mb_convert_encoding($message, 'UTF-8', 'GBK');
+                               }elseif ($body['Msg_Fmt'] == 0){
+                                   $message = $this->decode($message);
                                }
                                $sendData = [
                                    'mobile'  => trim($mobile),
