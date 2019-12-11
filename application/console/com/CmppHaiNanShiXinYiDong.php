85a2a642068d738fd1c1b1bb85a833f43f8d3903
wind-dust <1002380377@qq.com>
2019-12-11 17:59:00 +0800

    代码优化

--- application/console/com/CmppHaiNanShiXinYiDong.php
+++ application/console/com/CmppHaiNanShiXinYiDong.php
@@ -29,7 +29,7 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
             'Dest_Id'       => "", //短信接入码 短信端口号 服务代码
             'Sequence_Id'   => 1,
             'SP_ID'         => "",
-            'master_num'    => 3000,
+            'master_num'    => 1000,
         ];
 
     }
@@ -37,7 +37,6 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
     public function Send($content) {
         // $this->clientSocketInit();
         $redis = Phpredis::getConn();
-        // $a_time = 0;
         date_default_timezone_set('PRC');
         ini_set('memory_limit', '3072M'); // 临时设置最大内存占用为3G
         $content                    = 14;
@@ -52,8 +51,6 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
         //     'mar_task_id' => '',
         //     'content'     => '【冰雪传奇】尊敬的测评师：您账户已激活Vip5成功，赠GM号至尊礼包。领：http://uee.me/cF4vv。退订回T',
         // ]));
-        // print_r(json_encode(['mobile' => $mobile,'code' => $code]));die;
-        // $redis->rpush($redisMessageCodeSend,json_encode(['mobile' => $mobile,'code' => $code]));
         $socket   = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         $contdata = $this->content($content);
 
@@ -104,7 +101,6 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
                             $txt_head = 6;
                             $txt_len  = 140;
                             $max_len  = $txt_len - $txt_head;
-                            // $mobile   = 15201926171;
                             $code = $send_data['content']; //带签名
                             $uer_num    = 1; //本批接受信息的用户数量（一般小于100个用户，不同通道承载能力不同）
                             $timestring = time();
@@ -115,41 +111,34 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
                             if (strlen($code) > 140) {
                                 $pos          = 0;
                                 $num_messages = ceil(strlen($code) / $max_len);
-                                // echo $num_messages;die;
                                 for ($j = 0; $j < $num_messages; $j++) {
                                     $bodyData = pack("N", $num1) . pack("N", $num2);
-                                    $bodyData = $bodyData . pack('C', $num_messages); //Pk_total |Unsigned Integer |1 |相同 Msg_Id 的信息总条数，从 1 开始
-                                    $bodyData = $bodyData . pack('C', $j + 1); //Pk_number |Unsigned Integer |1 |相同 Msg_Id 的信息序号，从 1 开始
-                                    $bodyData = $bodyData . pack('C', 1); //Registered_Delivery |Unsigned Integer| 1| 是否要求返回状态确认报告： 0：不需要 1：需要 2：产生 SMC 话单 （该类型短信仅供网关计费使用，不发 送给目的终端)
-                                    $bodyData = $bodyData . pack('C', ''); //Msg_level |Unsigned Integer| 1 |信息级别
-                                    // $bodyData .= pack("a10", $Source_Addr); //可以为企业代码
-                                    $bodyData = $bodyData . pack("a10", $Service_Id); //Service_Id |Octet String| 10 |业务类型，是数字、字母和符号的组合。
-                                    $bodyData = $bodyData . pack('C', ''); //Fee_UserType  |Unsigned Integer | 1|计费用户类型字段 0：对目的终端 MSISDN 计费； 1：对源终端 MSISDN 计费； 2：对 SP 计费; 3：表示本字段无效，对谁计费参见 Fee_terminal_Id 字段。
-                                    $bodyData = $bodyData . pack("a21", $mobile); //Fee_terminal_Id |21 Unsigned Integer |被计费用户的号码（如本字节填空，则表 示本字段无效，对谁计费参见 Fee_UserType 字段，本字段与 Fee_UserType 字段互斥）
-                                    $bodyData = $bodyData . pack("C", 0); //TP_pId |1 |Unsigned Integer |GSM协议类型。详细是解释请参考 GSM03.40 中的 9.2.3.9
-                                    $bodyData = $bodyData . pack("C", 1); //TP_udhi |1 |Unsigned |Integer |GSM协议类型。详细是解释请参考 GSM03.40 中的 9.2.3.23,仅使用 1 位，右 对齐
-                                    $bodyData = $bodyData . pack("C", 15); //Msg_Fmt |1 |Unsigned |Integer |信息格式   0：ASCII 串   3：短信写卡操作   4：二进制信息   8：UCS2 编码 15：含 GBK 汉字(GBK编码内容与Msg_Fmt一致)
-
-                                    $bodyData = $bodyData . pack("a6", $Source_Addr); //Msg_src |6 |Octet String |信息内容来源(账号)
-                                    $bodyData = $bodyData . pack("a2", 02);
-                                    //FeeType |2 |Octet String |资费类别 01：对“计费用户号码”免费 02：对“计费用户号码”按条计信息费 03：对“计费用户号码”按包月收取信息 费 04：对“计费用户号码”的信息费封顶 05：对“计费用户号码”的收费是由 SP 实现
-                                    $bodyData = $bodyData . pack("a6", ''); // FeeCode |6 |Octet String |资费代码（以分为单位）
-                                    $bodyData = $bodyData . pack("a17", ''); //ValId_Time |17 |Octet |String 存活有效期，格式遵循 SMPP3.3 协议
-                                    $bodyData = $bodyData . pack("a17", ''); //At_Time |17 |Octet String |定时发送时间，格式遵循 SMPP3.3 协议
-                                    $bodyData = $bodyData . pack("a21", $Dest_Id); //Src_Id |21 |Octet String |源号码 SP 的服务代码或前缀为服务代码的长号 码, 网关将该号码完整的填到 SMPP 协议 Submit_SM消息相应的source_addr字段， 该号码最终在用户手机上显示为短消息 的主叫号码 (接入码)
-                                    $bodyData = $bodyData . pack("C", $uer_num); //DestUsr_tl |1 |Unsigned Integer |接收信息的用户数量(小于 100 个用户)
+                                    $bodyData.= pack('C', $num_messages);
+                                    $bodyData.= pack('C', $j + 1); 
+                                    $bodyData.= pack('C', 1);
+                                    $bodyData.= pack('C', ''); 
+                                    $bodyData.= pack("a10", $Service_Id);
+                                    $bodyData.= pack('C', ''); 
+                                    $bodyData.= pack("a21", $mobile);
+                                    $bodyData.= pack("C", 0); 
+                                    $bodyData.= pack("C", 1);
+                                    $bodyData.= pack("C", 15); 
+                                    $bodyData.= pack("a6", $Source_Addr);
+                                    $bodyData.= pack("a2", 02);
+                                    $bodyData.= pack("a6", ''); 
+                                    $bodyData.= pack("a17", '');
+                                    $bodyData.= pack("a17", ''); 
+                                    $bodyData.= pack("a21", $Dest_Id);
+                                    $bodyData.= pack("C", $uer_num);
                                     $p_n      = 21 * $uer_num;
-                                    $bodyData = $bodyData . pack("a" . $p_n, $mobile); //Dest_terminal_Id | 21*DestUsr_tl |Octet String |接收短信的 MSISDN 号码
-                                    // $len      = strlen($code);
+                                    $bodyData.= pack("a" . $p_n, $mobile);
                                     $udh     = pack("cccccc", 5, 0, 3, $Sequence_Id, $num_messages, $j + 1);
                                     $newcode = $udh . substr($code, $j * $max_len, $max_len);
                                     $len     = strlen($newcode);
-                                    // echo $len."\n";
-                                    $bodyData = $bodyData . pack("C", $len); //Msg_Length |1 |Unsigned Integer |信息长度(Msg_Fmt 值为 0 时：<160 个字 节；其它<=140 个字节)
-                                    $bodyData = $bodyData . pack("a" . $len, $newcode); // Msg_Content |Msg_length |Octet String |信息内容
-                                    $bodyData = $bodyData . pack("a8", ''); //Reserve | 8 | Octet String | 保留
+                                    $bodyData.= pack("C", $len);
+                                    $bodyData.= pack("a" . $len, $newcode);
+                                    $bodyData.= pack("a8", '');
                                     $Command_Id = 0x00000004; // 短信发送
-                                    $time = 0;
                                     $Total_Length = strlen($bodyData) + 12;
                                     $headData     = pack("NNN", $Total_Length, $Command_Id, $Sequence_Id);
                                     $redis->hset($redisMessageCodeSequenceId, $Sequence_Id, $send);
@@ -271,7 +260,6 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
                                             echo "激活测试应答" . "\n"; //激活测试,无消息体结构
                                         } else {
                                             echo "未声明head['Command_Id']:" . $head['Command_Id'];
-                                            // break;
                                         }
 
                                     }
@@ -283,90 +271,47 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
                                     $Sequence_Id = 1;
                                 }
                                 if ($i > $security_master) {
-                                    $time = 1;
                                     $i    = 0;
-                                }
-                                if ($time > 1) {
-                                    // usleep(10); //等待时间，进行下一次操作
                                 }
                                 continue;
                             } else { //单条短信
 
                                 $bodyData = pack("N", $num1) . pack("N", $num2);
-
-                                // $bodyData = (pack('I',pack("a8", $Msg_Id))); //Msg_Id |Unsigned Integer |8 | 信息标识，由 SP 侧短信网关本身产生， 本处填空
-                                $bodyData = $bodyData . pack('C', 1); //Pk_total |Unsigned Integer |1 |相同 Msg_Id 的信息总条数，从 1 开始
-                                $bodyData = $bodyData . pack('C', 1); //Pk_number |Unsigned Integer |1 |相同 Msg_Id 的信息序号，从 1 开始
-                                $bodyData = $bodyData . pack('C', 1); //Registered_Delivery |Unsigned Integer| 1| 是否要求返回状态确认报告： 0：不需要 1：需要 2：产生 SMC 话单 （该类型短信仅供网关计费使用，不发 送给目的终端)
-                                $bodyData = $bodyData . pack('C', ''); //Msg_level |Unsigned Integer| 1 |信息级别
-                                // $bodyData .= pack("a10", $Source_Addr); //可以为企业代码
-                                $bodyData = $bodyData . pack("a10", $Service_Id); //Service_Id |Octet String| 10 |业务类型，是数字、字母和符号的组合。
-                                $bodyData = $bodyData . pack('C', ''); //Fee_UserType  |Unsigned Integer | 1|计费用户类型字段 0：对目的终端 MSISDN 计费； 1：对源终端 MSISDN 计费； 2：对 SP 计费; 3：表示本字段无效，对谁计费参见 Fee_terminal_Id 字段。
-
-                                $bodyData = $bodyData . pack("a21", $mobile); //Fee_terminal_Id |21 Unsigned Integer |被计费用户的号码（如本字节填空，则表 示本字段无效，对谁计费参见 Fee_UserType 字段，本字段与 Fee_UserType 字段互斥）
-                                $bodyData = $bodyData . pack("C", 0); //TP_pId |1 |Unsigned Integer |GSM协议类型。详细是解释请参考 GSM03.40 中的 9.2.3.9
-
-                                /**
-                                 * TP_udhi ：0代表内容体里不含有协议头信息
-                                 * 1代表内容含有协议头信息（长短信，push短信等都是在内容体上含有头内容的）当设置内容体包含协议头
-                                 * ，需要根据协议写入相应的信息，长短信协议头有两种：
-                                 * 6位协议头格式：05 00 03 XX MM NN
-                                 * byte 1 : 05, 表示剩余协议头的长度
-                                 * byte 2 : 00, 这个值在GSM 03.40规范9.2.3.24.1中规定，表示随后的这批超长短信的标识位长度为1（格式中的XX值）。
-                                 * byte 3 : 03, 这个值表示剩下短信标识的长度
-                                 * byte 4 : XX，这批短信的唯一标志，事实上，SME(手机或者SP)把消息合并完之后，就重新记录，所以这个标志是否唯 一并不是很 重要。
-                                 * byte 5 : MM, 这批短信的数量。如果一个超长短信总共5条，这里的值就是5。
-                                 * byte 6 : NN, 这批短信的数量。如果当前短信是这批短信中的第一条的值是1，第二条的值是2。
-                                 * 例如：05 00 03 39 02 01
-                                 *
-                                 * 7 位的协议头格式：06 08 04 XX XX MM NN
-                                 * byte 1 : 06, 表示剩余协议头的长度
-                                 * byte 2 : 08, 这个值在GSM 03.40规范9.2.3.24.1中规定，表示随后的这批超长短信的标识位长度为2（格式中的XX值）。
-                                 * byte 3 : 04, 这个值表示剩下短信标识的长度
-                                 * byte 4-5 : XX
-                                 * XX，这批短信的唯一标志，事实上，SME(手机或者SP)把消息合并完之后，就重新记录，所以这个标志是否唯一并不是很重要。
-                                 * byte 6 : MM, 这批短信的数量。如果一个超长短信总共5条，这里的值就是5。
-                                 * byte 7 : NN, 这批短信的数量。如果当前短信是这批短信中的第一条的值是1，第二条的值是2。
-                                 * 例如：06 08 04 00 39 02 01
-                                 **/
-                                /* 字符串长度（包括中文）超出70字 为长短信 超过70字的，拆成多条发送，一般使用6位协议头，每条短信除去6字节协议头，剩余134字节存放剩余内容 */
-                                /* 一般在发长短信的时候，tp_udhi设置为1，然后短信内容需要拆分成多条，每条内容之前，加上协议头 */
-                                $bodyData = $bodyData . pack("C", 0); //TP_udhi |1 |Unsigned |Integer |GSM协议类型。详细是解释请参考 GSM03.40 中的 9.2.3.23,仅使用 1 位，右 对齐
-                                $bodyData = $bodyData . pack("C", 15); //Msg_Fmt |1 |Unsigned |Integer |信息格式   0：ASCII 串   3：短信写卡操作   4：二进制信息   8：UCS2 编码 15：含 GBK 汉字(GBK编码内容与Msg_Fmt一致)
-
-                                $bodyData = $bodyData . pack("a6", $Source_Addr); //Msg_src |6 |Octet String |信息内容来源(账号)
-                                $bodyData = $bodyData . pack("a2", 02);
-                                //FeeType |2 |Octet String |资费类别 01：对“计费用户号码”免费 02：对“计费用户号码”按条计信息费 03：对“计费用户号码”按包月收取信息 费 04：对“计费用户号码”的信息费封顶 05：对“计费用户号码”的收费是由 SP 实现
-                                $bodyData = $bodyData . pack("a6", ''); // FeeCode |6 |Octet String |资费代码（以分为单位）
-                                $bodyData = $bodyData . pack("a17", ''); //ValId_Time |17 |Octet |String 存活有效期，格式遵循 SMPP3.3 协议
-                                $bodyData = $bodyData . pack("a17", ''); //At_Time |17 |Octet String |定时发送时间，格式遵循 SMPP3.3 协议
-                                $bodyData = $bodyData . pack("a21", $Dest_Id); //Src_Id |21 |Octet String |源号码 SP 的服务代码或前缀为服务代码的长号 码, 网关将该号码完整的填到 SMPP 协议 Submit_SM消息相应的source_addr字段， 该号码最终在用户手机上显示为短消息 的主叫号码 (接入码)
-                                $bodyData = $bodyData . pack("C", $uer_num); //DestUsr_tl |1 |Unsigned Integer |接收信息的用户数量(小于 100 个用户)
+                                $bodyData.= pack('C', 1);
+                                $bodyData.= pack('C', 1);
+                                $bodyData.= pack('C', 1);
+                                $bodyData.= pack('C', '');
+                                $bodyData.= pack("a10", $Service_Id);
+                                $bodyData.= pack('C', '');
+                                $bodyData.= pack("a21", $mobile); 
+                                $bodyData.= pack("C", 0);
+                                $bodyData.= pack("C", 0);
+                                $bodyData.= pack("C", 15);
+                                $bodyData.= pack("a6", $Source_Addr);
+                                $bodyData.= pack("a2", 02);
+                                $bodyData.= pack("a6", '');
+                                $bodyData.= pack("a17", '');
+                                $bodyData.= pack("a17", '');
+                                $bodyData.= pack("a21", $Dest_Id);
+                                $bodyData.= pack("C", $uer_num);
                                 $p_n      = 21 * $uer_num;
-                                $bodyData = $bodyData . pack("a" . $p_n, $mobile); //Dest_terminal_Id | 21*DestUsr_tl |Octet String |接收短信的 MSISDN 号码
+                                $bodyData.= pack("a" . $p_n, $mobile);
                                 $len      = strlen($code);
-                                $bodyData = $bodyData . pack("C", $len); //Msg_Length |1 |Unsigned Integer |信息长度(Msg_Fmt 值为 0 时：<160 个字 节；其它<=140 个字节)
-                                $bodyData = $bodyData . pack("a" . $len, $code); // Msg_Content |Msg_length |Octet String |信息内容
-                                $bodyData = $bodyData . pack("a8", ''); //Reserve | 8 | Octet String | 保留
-                                // $bodyData = $bodyData . pack('I',pack("a8", '')); //Reserve |8 |Octet String |保留
-
+                                $bodyData.= pack("C", $len); 
+                                $bodyData.= pack("a" . $len, $code);
+                                $bodyData.= pack("a8", ''); 
                                 $Command_Id = 0x00000004; // 短信发送
-                                // $Sequence_Id = $i;
                                 $time = 0;
-
                             }
                             if ($i > $security_master) {
                                 $time = 1;
                                 $i    = 0;
                             }
                             $redis->hset($redisMessageCodeSequenceId, $Sequence_Id, $send);
-                        } else {
+                        } else {//没有号码发送时 发送连接请求
                             $bodyData    = pack("a6a16CN", $Source_Addr, $AuthenticatorSource, $Version, $Timestamp);
                             $Command_Id  = 0x00000008; //保持连接
-                            $Sequence_Id = $i;
-                            $time        = 15;
                         }
-                        //没有号码发送时 发送连接请求
                     }
                     $Total_Length = strlen($bodyData) + 12;
                     $headData     = pack("NNN", $Total_Length, $Command_Id, $Sequence_Id);
