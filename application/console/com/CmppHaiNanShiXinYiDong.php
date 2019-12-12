d20f736dca8c35b2077f996f03fa22a48f64fd5a
wind-dust <1002380377@qq.com>
2019-12-12 14:51:43 +0800

    代码提交

--- application/console/com/CmppHaiNanShiXinYiDong.php
+++ application/console/com/CmppHaiNanShiXinYiDong.php
@@ -46,11 +46,11 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
         // $redisMessageCodeDeliver    = 'index:meassage:code:deliver:' . $content; //行业通知MsgId
         $redisMessageCodeDeliver = 'index:meassage:code:new:deliver:' . $content; //行业通知MsgId
 
-        $send = $redis->rPush($redisMessageCodeSend, json_encode([
-            'mobile'      => '15821193682',
-            'mar_task_id' => '',
-            'content'     => '【冰封传奇】已为您发出688888元宝和VIP满级号，今日限领至尊屠龙！戳 https://ltv7.cn/45RHD 回T退订',
-        ]));
+        // $send = $redis->rPush($redisMessageCodeSend, json_encode([
+        //     'mobile'      => '15821193682',
+        //     'mar_task_id' => '',
+        //     'content'     => '【冰封传奇】已为您发出688888元宝和VIP满级号，今日限领至尊屠龙！戳 https://ltv7.cn/45RHD 回T退订',
+        // ]));
         $socket   = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         $contdata = $this->content($content);
 
