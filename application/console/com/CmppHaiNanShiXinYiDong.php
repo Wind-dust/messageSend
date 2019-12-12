6c041d3bdaced746a4577fdb8dded6560406ac4d
wind-dust <1002380377@qq.com>
2019-12-12 15:32:35 +0800

    代码调整

--- application/console/com/CmppHaiNanShiXinYiDong.php
+++ application/console/com/CmppHaiNanShiXinYiDong.php
@@ -411,7 +411,7 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
                             $contentlen = $head['Total_Length'] - 65 - 12;
                             $body        = unpack("N2Msg_Id/a21Dest_Id/a10Service_Id/CTP_pid/CTP_udhi/CMsg_Fmt/a21Src_terminal_Id/CRegistered_Delivery/CMsg_Length/a" . $contentlen . "Msg_Content/", $bodyData);
                             $Msg_Content = unpack("N2Msg_Id/a7Stat/a10Submit_time/a10Done_time/", $body['Msg_Content']);
-
+                            print_r($Msg_Content);
                             $mesage = $redis->hget($redisMessageCodeMsgId, $Msg_Content['Msg_Id1'] . $Msg_Content['Msg_Id2']);
                             if ($mesage) {
                                 $redis->hdel($redisMessageCodeMsgId, $body['Msg_Id1'] . $body['Msg_Id2']);
