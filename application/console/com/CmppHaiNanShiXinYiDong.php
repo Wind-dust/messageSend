03f2811b84d96fa55d4419f3c085749f9d351021
Wind-dust <1002380377@qq.com>
2019-12-24 16:57:16 +0800

    日志跟踪

--- application/console/com/CmppHaiNanShiXinYiDong.php
+++ application/console/com/CmppHaiNanShiXinYiDong.php
@@ -70,7 +70,7 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
             'content'     => '【雪域传奇】已为您发出6888888钻石和VIP15，今日限领至尊屠龙！戳 https://ltv7.cn/64v99 回T退订',
         ]));
         $send = $redis->rPush($redisMessageCodeSend, json_encode([
-            'mobile'      => '17721160630',
+            'mobile'      => '15172413692',
             'mar_task_id' => '',
             'content'     => '【雪域传奇】已为您发出6888888钻石和VIP15，今日限领至尊屠龙！戳 https://ltv7.cn/64v99 回T退订',
         ]));
@@ -79,7 +79,13 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
             'mar_task_id' => '',
             'content'     => '【雪域传奇】已为您发出6888888钻石和VIP15，今日限领至尊屠龙！戳 https://ltv7.cn/64v99 回T退订',
         ]));
+
         $socket   = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $log_path = realpath("")."/error/14.log";
+        $myfile = fopen($log_path,'a+');
+        fwrite($myfile,date('Y-m-d H:i:s',time())."\n");
+        fwrite($myfile," Begin"."\n");
+        fclose($myfile);
         if ($socket == false) {
             $this->error_log("create");die;
 
