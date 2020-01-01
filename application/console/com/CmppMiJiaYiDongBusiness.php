ca767b9869e405963250e88227c29aa4496c8350
Wind-dust <1002380377@qq.com>
2020-01-01 12:18:36 +0800

    代码调试

--- application/console/com/CmppMiJiaYiDongBusiness.php
+++ application/console/com/CmppMiJiaYiDongBusiness.php
@@ -50,7 +50,7 @@ class CmppMiJiaYiDongBusiness extends Pzlife {
          $send = $redis->rPush($redisMessageCodeSend, json_encode([
             'mobile'      => '15201926171',
             'mar_task_id' => '',
-            'content'     => '感谢您对于CellCare的信赖和支持，为了给您带来更好的服务体验，特邀您针对本次服务进行评价https://www.wenjuan.com/s/6rqIZz/ ，请您在24小时内提交此问卷，谢谢配合。期待您的反馈！如需帮助，敬请致电400-8206-142【美丽田园】',
+            'content'     => '验证码为156023【美丽田园】',
         ]));
         
         $socket   = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
