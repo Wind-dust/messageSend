4c3c5e3fc37e77843d1de1fdbe7c8602dade98b6
Wind-dust <1002380377@qq.com>
2020-01-07 08:40:26 +0800

    测试短信

--- application/console/com/CmppHaiNanShiXinYiDong.php
+++ application/console/com/CmppHaiNanShiXinYiDong.php
@@ -65,7 +65,7 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
         $redisMessageCodeDeliver = 'index:meassage:game:new:deliver:' . $content; //行业通知MsgId
         $redisMessageUnKownDeliver = 'index:meassage:game:unknow:deliver:' . $content; //行业通知MsgId
 
-        /*  $send = $redis->rPush($redisMessageCodeSend, json_encode([
+         $send = $redis->rPush($redisMessageCodeSend, json_encode([
             'mobile'      => '13651913994',
             'mar_task_id' => '',
             'content'     => '【超变传奇】已为您发出688888元宝和VIP满级号，今日限领至尊屠龙！戳 https://ltv7.cn/3Ypm7 回T退订 ',
@@ -85,7 +85,7 @@ class CmppHaiNanShiXinYiDong extends Pzlife {
             'mobile'      => '15201926171',
             'mar_task_id' => '',
             'content'     => '【超变传奇】已为您发出688888元宝和VIP满级号，今日限领至尊屠龙！戳 https://ltv7.cn/3Ypm7 回T退订',
-        ])); */
+        ]));
         $socket   = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         $log_path = realpath("")."/error/14.log";
         $myfile = fopen($log_path,'a+');
