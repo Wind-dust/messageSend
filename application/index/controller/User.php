880326714eac92013911f270479f51a26a1b4182
Wind-dust <1002380377@qq.com>
2020-01-09 12:13:41 +0800

    文档更新

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -574,6 +574,7 @@ class User extends MyController
      * @apiParam (入参) {String} con_id
      * @apiParam (入参) {String} page 
      * @apiParam (入参) {String} pageNum 
+     * @apiParam (入参) {String} business_id 5,6,7,9 
      * @apiSuccess (返回) {String} code 200:成功  3001:logo为空或者未上传成功/ 3002:business错误 / 3003:用户不存在 / 3004:登录失败
      * @apiSuccess (返回) {Array} data 用户信息
      * @apiSampleRequest /index/user/getUserModel
