139478b29955be8e111e9ef6ef2ea3db1e510ef6
Wind-dust <1002380377@qq.com>
2020-01-14 18:06:55 +0800

    代码提交

--- extend/cache/Phpredis.php
+++ extend/cache/Phpredis.php
@@ -5,18 +5,22 @@ namespace cache;
 use Config;
 use \Redis;
 
-class Phpredis {
+class Phpredis
+{
     private static $conn;    // 单例redis连接
     private $redis_config;   // 配置config/redis.php
 
-    private function __construct() {
+    private function __construct()
+    {
     }
 
-    private function __clone() {
+    private function __clone()
+    {
     }
 
     // 连接redis
-    private function connect() {
+    private function connect()
+    {
         if (class_exists('Redis')) {
             $this->loadConfig();
             $redis = new Redis();
@@ -24,15 +28,16 @@ class Phpredis {
             if (!empty($this->redis_config['password'])) {
                 $redis->auth($this->redis_config["password"]);
             }
-            if (!empty($this->redis_config['select'])) {
-                $redis->select($this->redis_config['select']);
-            }
+            // if (!empty($this->redis_config['select'])) {
+            //     $redis->select($this->redis_config['select']);
+            // }
             self::$conn = $redis;
         }
     }
 
     // 获取已存在的redis连接
-    static public function getConn() {
+    static public function getConn()
+    {
         if (!self::$conn instanceof Redis) {
             $phpredis = new self();
             $phpredis->connect();
@@ -41,7 +46,8 @@ class Phpredis {
     }
 
     // 载入配置
-    private function loadConfig() {
+    private function loadConfig()
+    {
         $this->redis_config = Config::get('cache.redis');
     }
 }
