4ffaa005452b1e991a19a66c3a230ec7ff706107
Wind-dust <1002380377@qq.com>
2020-02-04 10:53:11 +0800

    代码注释提交

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -752,9 +752,9 @@ class CmppCreateCodeTask extends Pzlife
         ini_set('memory_limit', '3072M'); // 临时设置最大内存占用为3G
         // date_default_timezone_set('PRC');
         $redisMessageMarketingSend = 'index:meassage:business:sendtask';
-        for ($i = 173993; $i < 174487; $i++) {
-            $this->redis->rPush('index:meassage:business:sendtask', $i);
-        }
+        // for ($i = 173993; $i < 174487; $i++) {
+        //     $this->redis->rPush('index:meassage:business:sendtask', $i);
+        // }
 
         $push_messages = []; //推送队列
         $rollback = [];
