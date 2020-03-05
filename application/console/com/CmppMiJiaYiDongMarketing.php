28497c17deea0df286bbe1d61b0409ea3d65e8f6
Wind-dust <1002380377@qq.com>
2020-03-05 11:51:27 +0800

    通道修正

--- application/console/com/CmppMiJiaYiDongMarketing.php
+++ application/console/com/CmppMiJiaYiDongMarketing.php
@@ -379,7 +379,7 @@ class CmppMiJiaYiDongMarketing extends Pzlife
                                         } else {
                                             $Msg_Content = unpack("N2Msg_Id/a" . $stalen . "Stat/a10Submit_time/a10Done_time/a21Dest_terminal_Id/NSMSC_sequence", $body['Msg_Content']);
                                         }
-
+                                        print_r($Msg_Content);
                                         $mesage = $redis->hget($redisMessageCodeMsgId, $Msg_Content['Msg_Id1'] . $Msg_Content['Msg_Id2']);
                                         if ($mesage) {
                                             $redis->hdel($redisMessageCodeMsgId, $body['Msg_Id1'] . $body['Msg_Id2']);
@@ -387,20 +387,18 @@ class CmppMiJiaYiDongMarketing extends Pzlife
                                             $mesage                = json_decode($mesage, true);
                                             $mesage['Stat']        = $Msg_Content['Stat'];
                                             // $mesage['Msg_Id']        = $Msg_Content['Msg_Id1'] . $Msg_Content['Msg_Id2'];
-                                            $mesage['Submit_time'] = $Msg_Content['Submit_time'];
-                                            $mesage['Done_time']   = $Msg_Content['Done_time'];
+                                            $mesage['Submit_time'] = isset($Msg_Content['Submit_time']) ? $Msg_Content['Submit_time'] : date('ymdHis', $mesage['my_submit_time']);
+                                            $mesage['Done_time']   = isset($Msg_Content['Done_time']) ? $Msg_Content['Done_time'] : date('ymdHis', time());
                                             $mesage['receive_time'] = time(); //回执时间戳
                                             $redis->rpush($redisMessageCodeDeliver, json_encode($mesage));
                                         } else { //不在记录中的回执存入缓存，
-
-                                            // print_r($body);
-                                            // print_r($Msg_Content);
-                                            $mesage['Stat']        = $Msg_Content['Stat'];
-                                            $mesage['Submit_time'] = $Msg_Content['Submit_time'];
-                                            $mesage['Done_time']   = $Msg_Content['Done_time'];
+                                            $mesage['Stat']        = isset($Msg_Content['Stat']) ? $Msg_Content['Stat'] : 'UNKNOWN';
+                                            $mesage['Submit_time'] = trim(isset($Msg_Content['Submit_time']) ? $Msg_Content['Submit_time'] : date('ymdHis', time()));
+                                            $mesage['Done_time']   = trim(isset($Msg_Content['Done_time']) ? $Msg_Content['Done_time'] : date('ymdHis', time()));
                                             // $mesage['mobile']      = $body['Dest_Id '];//手机号
-                                            $mesage['mobile']   = trim($Msg_Content['Dest_terminal_Id']);
+                                            $mesage['mobile']   = isset($Msg_Content['Dest_terminal_Id']) ? $Msg_Content['Dest_terminal_Id'] : '';
                                             $mesage['receive_time'] = time(); //回执时间戳
+                                            $mesage['Msg_Id']   = $Msg_Content['Msg_Id1'] . $Msg_Content['Msg_Id2'];
                                             $redis->rPush($redisMessageUnKownDeliver, json_encode($mesage));
                                         }
                                         $callback_Command_Id = 0x80000005;
