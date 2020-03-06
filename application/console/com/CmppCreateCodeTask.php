13b95b60e31789215978736656fb92759ae11228
Wind-dust <1002380377@qq.com>
2020-03-06 11:01:20 +0800

    代码修正

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -2608,10 +2608,10 @@ Db::rollback();
                             exception($e);
                         }
                     } else {
-                        if ($sendtasklog[0]['create_time'] > $time) {
-                            $redis->rpush('index:meassage:code:cms:deliver:' . $channel_id, $sendlog);
-                            exit('today is success');
-                        }
+                        // if ($sendtasklog[0]['create_time'] > $time) {
+                        //     $redis->rpush('index:meassage:code:cms:deliver:' . $channel_id, $sendlog);
+                        //     exit('today is success');
+                        // }
                         if (strpos($send_log['content'], '问卷') !== false) {
                             $status_message = 'DELIVRD';
                         } else {
