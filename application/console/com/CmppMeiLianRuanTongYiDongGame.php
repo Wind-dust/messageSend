d4beb54fe147cf09c7697f81a5d05b9cb0a2be9d
wind-dust <1002380377@qq.com>
2020-04-22 19:09:05 +0800

    错误码提交

--- application/console/com/CmppMeiLianRuanTongYiDongGame.php
+++ application/console/com/CmppMeiLianRuanTongYiDongGame.php
@@ -141,7 +141,7 @@ class CmppMeiLianRuanTongYiDongGame extends Pzlife
                         }
                         //通道断口处理
                         if ($body['Status'] != 0) {
-                            exit($error_msg);
+                            exit($error_msg.":".$verify_status);
                         }
                     } else if ($head['Command_Id'] == 0x80000004) {
                         $body = unpack("N2Msg_Id/CResult", $bodyData);
