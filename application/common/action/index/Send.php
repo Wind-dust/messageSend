3901ba6ba8a87262439e4cf182c9ec96a5693714
wind-dust <1002380377@qq.com>
2020-04-26 15:03:45 +0800

    切回蓝鲸

--- application/common/action/index/Send.php
+++ application/common/action/index/Send.php
@@ -480,9 +480,6 @@ return $result;
             } else {
                 $data['channel_id'] = 22; //蓝鲸
             }
-            if ($user['id'] == 47) {
-                $data['channel_id'] = 24;
-            }
         }
         Db::startTrans();
         try {
