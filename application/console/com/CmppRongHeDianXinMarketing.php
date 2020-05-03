42b29824f1e578e7d82811c8b027654f10cc02f5
wind-dust <1002380377@qq.com>
2020-05-03 18:34:49 +0800

    融合修正

--- application/console/com/CmppRongHeDianXinMarketing.php
+++ application/console/com/CmppRongHeDianXinMarketing.php
@@ -25,9 +25,9 @@ class CmppRongHeDianXinMarketing extends Pzlife
         return [
             'host'          => "39.98.238.240", //服务商ip
             'port'          => "7890", //短连接端口号   17890长连接端口号
-            'Source_Addr'   => "922212", //企业id  企业代码
-            'Shared_secret' => '453450', //网关登录密码
-            'Service_Id'    => "922212", //业务代码
+            'Source_Addr'   => "269512", //企业id  企业代码
+            'Shared_secret' => '613472', //网关登录密码
+            'Service_Id'    => "269512", //业务代码
             'template_id'   => "", //模板id
             'Dest_Id'       => "", //短信接入码 短信端口号 服务代码
             'Sequence_Id'   => 1,
