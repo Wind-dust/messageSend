d21c81263705a9b6ab67a589a1176b58d5e9d676
wind-dust <1002380377@qq.com>
2020-05-11 16:21:23 +0800

    回复提交

--- application/console/com/CmppBiShangYiDongBusiness.php
+++ application/console/com/CmppBiShangYiDongBusiness.php
@@ -434,9 +434,9 @@ class CmppBiShangYiDongBusiness extends Pzlife {
                                         print_r($mesage);
                                         $callback_Command_Id = 0x80000005;
 
-                                        // $new_body         = pack("N", $body['Msg_Id1']) . pack("N", $body['Msg_Id2']) . pack("C", $Result);
-                                        // $new_Total_Length = strlen($new_body) + 12;
-                                        // $new_headData     = pack("NNN", $new_Total_Length, $callback_Command_Id, $body['Msg_Id2']);
+                                        $new_body         = pack("N", $body['Msg_Id1']) . pack("N", $body['Msg_Id2']) . pack("C", $Result);
+                                        $new_Total_Length = strlen($new_body) + 12;
+                                        $new_headData     = pack("NNN", $new_Total_Length, $callback_Command_Id, $body['Msg_Id2']);
                                         $receipts = [];
                                         $receipts = [
                                             'Msg_Id1' => $body['Msg_Id1'],
@@ -446,7 +446,7 @@ class CmppBiShangYiDongBusiness extends Pzlife {
                                         ];
                                         $receipt_data[] = $receipts;
                                         
-                                        // socket_write($socket, $new_headData . $new_body, $new_Total_Length);
+                                        socket_write($socket, $new_headData . $new_body, $new_Total_Length);
                                         // $huifu = 1;
                                     } else if ($head['Command_Id'] == 0x00000008) {
                                         echo "心跳维持中" . "\n"; //激活测试,无消息体结构
@@ -596,7 +596,7 @@ class CmppBiShangYiDongBusiness extends Pzlife {
                                          $new_body         = pack("N", $value['Msg_Id1']) . pack("N", $value['Msg_Id2']) . pack("C", $value['Result']);
                                          $new_Total_Length = strlen($new_body) + 12;
                                          $new_headData     = pack("NNN", $new_Total_Length,  0x80000005, $value['Msg_Id2']);
-                                         socket_write($socket, $new_headData . $new_body, $new_Total_Length);
+                                        //  socket_write($socket, $new_headData . $new_body, $new_Total_Length);
                                      }
                                     //  sleep(1);
                                     
