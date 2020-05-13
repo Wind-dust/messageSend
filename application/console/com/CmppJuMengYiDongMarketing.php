95dc31b04f55fbeb4a93d755390aa482e34b536a
wind-dust <1002380377@qq.com>
2020-05-13 11:21:12 +0800

    通道接入码调整

--- application/console/com/CmppJuMengYiDongMarketing.php
+++ application/console/com/CmppJuMengYiDongMarketing.php
@@ -47,7 +47,7 @@ class CmppJuMengYiDongMarketing extends Pzlife
             'Shared_secret' => '123ABC', //网关登录密码
             'Service_Id'    => "440667", //业务代码
             'template_id'   => "", //模板id
-            'Dest_Id'       => "1069610666", //短信接入码 短信端口号 服务代码
+            'Dest_Id'       => "1069440667", //短信接入码 短信端口号 服务代码
             'Sequence_Id'   => 1,
             'SP_ID'         => "",
             'master_num'    => 300,
