095177838e82ada6dcc973181c84c0d989640860
wind-dust <1002380377@qq.com>
2020-05-17 10:14:31 +0800

    融合通道调试

--- application/console/com/CmppRongHeDianXinMarketing.php
+++ application/console/com/CmppRongHeDianXinMarketing.php
@@ -146,7 +146,7 @@ class CmppRongHeDianXinMarketing extends Pzlife
                         }
                         //通道断口处理
                         if ($body['Status'] != 0) {
-                            exit($error_msg);
+                            exit($error_msg."错误码：".$body['Status']);
                         }
                     } else if ($head['Command_Id'] == 0x80000004) {
                         $body = unpack("N2Msg_Id/CResult", $bodyData);
@@ -319,7 +319,7 @@ class CmppRongHeDianXinMarketing extends Pzlife
                                         }
                                         //通道断口处理
                                         if ($body['Status'] != 0) {
-                                            exit($error_msg."错误码：".$verify_status);
+                                            exit($error_msg."错误码：".$body['Status']);
                                         }
                                     } else if ($head['Command_Id'] == 0x80000004) {
                                         $body = unpack("N2Msg_Id/CResult", $bodyData);
