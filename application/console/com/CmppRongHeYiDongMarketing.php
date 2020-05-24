5257552e4f5bd7b55f60ac30a5e1f122553562b8
wind-dust <1002380377@qq.com>
2020-05-24 12:33:11 +0800

    错误代码抛出

--- application/console/com/CmppRongHeYiDongMarketing.php
+++ application/console/com/CmppRongHeYiDongMarketing.php
@@ -147,7 +147,7 @@ class CmppRongHeYiDongMarketing extends Pzlife
                         }
                         //通道断口处理
                         if ($body['Status'] != 0) {
-                            exit($error_msg);
+                            exit("其他错误，错误代码：【".$body['Result']."】\n");
                         }
                     } else if ($head['Command_Id'] == 0x80000004) {
                         $body = unpack("N2Msg_Id/CResult", $bodyData);
