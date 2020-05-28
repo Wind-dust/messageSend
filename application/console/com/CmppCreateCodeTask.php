78a7f1c84fa5cdfbe8693a091d946e948512b55a
wind-dust <1002380377@qq.com>
2020-05-28 12:17:25 +0800

    短信发送

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -5689,25 +5689,25 @@ class CmppCreateCodeTask extends Pzlife
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 90669);
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 90672);
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 90679); */
-        $this->redis->rpush('index:meassage:sflmessage:sendtask', 90695);
+        // $this->redis->rpush('index:meassage:sflmessage:sendtask', 90695);
         /* $this->redis->rpush('index:meassage:sflmessage:sendtask', 73735);
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 73740);
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 73754);
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 73755);
         $this->redis->rpush('index:meassage:sflmessage:sendtask', 73764); */
         $tody_time = strtotime(date("Ymd",time()));
-        // try {
-        //     $mysql_connect->table('yx_sfl_send_task')->where([['create_time','>',$tody_time]])->update(['free_trial' => 2, 'yidong_channel_id' => 83, 'liantong_channel_id' => 84, 'dianxin_channel_id' => 84]);
-        //     /* $where = [];
-        //     $where = [['create_time','>',$tody_time],['template_id', '<>','100150821']];
-        //     $mysql_connect->table('yx_sfl_send_task')->where($where)->update(['free_trial' => 2, 'yidong_channel_id' => 86, 'liantong_channel_id' => 88, 'dianxin_channel_id' => 87]);*/
-        //     $sendid = $mysql_connect->query("SELECT `id` FROM yx_sfl_send_task WHERE `id` > 90719 AND `create_time` >  ".$tody_time ); 
-        //     foreach ($sendid as $key => $value) {
-        //         $this->redis->rpush('index:meassage:sflmessage:sendtask', $value['id']);
-        //     }
-        // } catch (\Exception $th) {
-        //     exception($th);
-        // }
+        try {
+            $mysql_connect->table('yx_sfl_send_task')->where([['create_time','>',$tody_time]])->update(['free_trial' => 2, 'yidong_channel_id' => 83, 'liantong_channel_id' => 84, 'dianxin_channel_id' => 84]);
+            /* $where = [];
+            $where = [['create_time','>',$tody_time],['template_id', '<>','100150821']];
+            $mysql_connect->table('yx_sfl_send_task')->where($where)->update(['free_trial' => 2, 'yidong_channel_id' => 86, 'liantong_channel_id' => 88, 'dianxin_channel_id' => 87]);*/
+            $sendid = $mysql_connect->query("SELECT `id` FROM yx_sfl_send_task WHERE `id` > 90719 AND `create_time` >  ".$tody_time ); 
+            foreach ($sendid as $key => $value) {
+                $this->redis->rpush('index:meassage:sflmessage:sendtask', $value['id']);
+            }
+        } catch (\Exception $th) {
+            exception($th);
+        }
         $deduct = 1; //1扣量,2不扣
         $rate = 40;
         $white_list = [
