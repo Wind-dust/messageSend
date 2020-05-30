5df7ffeba4b4ef3a5eed383ada9e6d68c69a542b
wind-dust <1002380377@qq.com>
2020-05-30 16:58:45 +0800

    移动营销通道提速

--- application/console/com/CmppJuMengYiDongMarketing.php
+++ application/console/com/CmppJuMengYiDongMarketing.php
@@ -438,6 +438,7 @@ class CmppJuMengYiDongMarketing extends Pzlife
                                         $new_headData     = pack("NNN", $new_Total_Length, $callback_Command_Id, $head['Sequence_Id']);
                                         socket_write($socket, $new_headData . $new_body, $new_Total_Length);
                                         $receive = 2;
+                                        usleep(30);
                                     } else if ($head['Command_Id'] == 0x00000008) {
                                         echo "心跳维持中" . "\n"; //激活测试,无消息体结构
                                     } else if ($head['Command_Id'] == 0x80000008) {
@@ -513,7 +514,7 @@ class CmppJuMengYiDongMarketing extends Pzlife
                                         $headData     = pack("NNN", $Total_Length, $Command_Id, $Sequence_Id);
                                         $send_data['my_submit_time'] = time(); //发送时间戳
                                         $redis->hset($redisMessageCodeSequenceId, $Sequence_Id, json_encode($send_data));
-                                        usleep(300);
+                                        usleep(160);
                                         socket_write($socket, $headData . $bodyData, $Total_Length);
                                         $send_status = 2;
                                         ++$i;
@@ -570,7 +571,7 @@ class CmppJuMengYiDongMarketing extends Pzlife
                                     socket_write($socket, $headData . $bodyData, $Total_Length);
 
                                     $send_status = 2;
-                                    usleep(300);
+                                    usleep(160);
                                 }
                             } else { //心跳
                                 $Command_Id  = 0x00000008; //保持连接
