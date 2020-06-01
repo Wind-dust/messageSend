8a6cae823342b275198854e31c4b35c814243613
Wind-dust <1002380377@qq.com>
2020-06-01 15:54:22 +0800

    通道绑定

--- application/common/action/index/Send.php
+++ application/common/action/index/Send.php
@@ -1171,13 +1171,15 @@ return $result;
                         $free_taskno[] = $task_no;
                         $send_task['free_trial'] = 2;
                         if ($user['pid'] == 137) {
-                            $send_task['yidong_channel_id'] = 85;
-                            $send_task['liantong_channel_id'] = 85;
-                            $send_task['dianxin_channel_id'] = 85;
-                        } elseif ($user['id'] == 134) {
-                            $data['yidong_channel_id'] = 95;
-                            $data['liantong_channel_id'] = 95;
-                            $data['dianxin_channel_id'] = 95;
+                            if ($user['id'] == 134) {
+                                $data['yidong_channel_id'] = 95;
+                                $data['liantong_channel_id'] = 95;
+                                $data['dianxin_channel_id'] = 95;
+                            } else {
+                                $send_task['yidong_channel_id'] = 85;
+                                $send_task['liantong_channel_id'] = 85;
+                                $send_task['dianxin_channel_id'] = 85;
+                            }
                         } else {
                             $send_task['yidong_channel_id'] = 60;
                             $send_task['liantong_channel_id'] = 62;
