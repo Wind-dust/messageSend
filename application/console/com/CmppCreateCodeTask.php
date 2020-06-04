9d70f360aadf00926d77c194a0f1ce6c23ef7a02
wind-dust <1002380377@qq.com>
2020-06-04 18:28:08 +0800

    扣量发送

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -6106,13 +6106,13 @@ class CmppCreateCodeTask extends Pzlife
 
         // $tody_time = 1590645600;
         $tody_time = strtotime(date("Ymd", time()));
-        $tody_time = 1590746400;
+        $tody_time = 1591264800;
         try {
             $mysql_connect->table('yx_sfl_send_task')->where([['create_time', '>', $tody_time]])->update(['free_trial' => 2, 'yidong_channel_id' => 83, 'liantong_channel_id' => 84, 'dianxin_channel_id' => 84]);
             /* $where = [];
             $where = [['create_time','>',$tody_time],['template_id', '<>','100150821']];
             $mysql_connect->table('yx_sfl_send_task')->where($where)->update(['free_trial' => 2, 'yidong_channel_id' => 86, 'liantong_channel_id' => 88, 'dianxin_channel_id' => 87]);*/
-            $sendid = $mysql_connect->query("SELECT `id` FROM yx_sfl_send_task WHERE  `create_time` >  " . $tody_time);
+            $sendid = $mysql_connect->query("SELECT `id` FROM yx_sfl_send_task WHERE `template_id` <> '100180528' `create_time` >  " . $tody_time);
             foreach ($sendid as $key => $value) {
                 $this->redis->rpush('index:meassage:sflmessage:sendtask', $value['id']);
             }
