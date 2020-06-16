2a4f1627054de60ed48904c781128489c25ff22b
wind-dust <1002380377@qq.com>
2020-06-16 23:48:00 +0800

    代码调试

--- application/console/com/CmppMiaoXinYiDongBusiness.php
+++ application/console/com/CmppMiaoXinYiDongBusiness.php
@@ -85,11 +85,11 @@ class CmppMiaoXinYiDongBusiness extends Pzlife
             // 'content'     => '【沙驰服饰】“衬”现在，全身心去爱。沙驰衬衫节👔礼献父亲节，充值赠好礼，全场VIP折上8折起，快来表达你对爸爸👨的爱吧😄。退订回T',
             'content'     => '【钰晰科技】😄您本次登录的验证码为0518，回复QX取消本次登录',
         ]));
-        $code = '【钰晰科技】😄您本次登录的验证码为0518，回复QX取消本次登录';
+        // $code = '【钰晰科技】😄您本次登录的验证码为0518，回复QX取消本次登录';
         // $code = mb_convert_encoding('【钰晰科技】😄您本次登录的验证码为0518，回复QX取消本次登录', 'UCS-2', 'UTF-8');
-        $code =iconv("UTF-8","UCS-2//TRANSLIT",$code);
+        // $code =iconv("UTF-8","UCS-2//TRANSLIT",$code);
         // $code =iconv("UCS-2","UTF-8",$code);
-        print_r($code);die;
+        // print_r($code);die;
         $socket   = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         $log_path = realpath("") . "/error/".$content.".log";
         $myfile = fopen($log_path, 'a+');
@@ -490,7 +490,7 @@ class CmppMiaoXinYiDongBusiness extends Pzlife
                                 $num2 = substr($timestring, 8) . $this->combination($i);
                                 // $code = mb_convert_encoding($code, 'GBK', 'UTF-8');
                                 // $code = mb_convert_encoding($code, 'UCS-2', 'UTF-8');
-                                $code =iconv("UTF-8","UCS-2",$code);
+                                $code =iconv("UTF-8","UCS-2//TRANSLIT",$code);
                                 // iconv("UTF-8","gbk",$code);
                                 // $redis->rPush($redisMessageCodeSend, json_encode($send_data));
                                 // // print_r($code);die;
