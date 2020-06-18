1996dedc51b57227f6194a477a98a0424e464678
wind-dust <1002380377@qq.com>
2020-06-18 16:34:44 +0800

    彩信发送

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -8574,6 +8574,7 @@ class CmppCreateCodeTask extends Pzlife
             13213032008
         ];
         $tody_time = strtotime(date("Ymd", time()));
+        $tody_time = 1592460000;
         try {
             /* $mysql_connect->query("UPDATE yx_sfl_multimedia_message SET `free_trial` = 2 AND `yidong_channel_id` = 94 AND `liantong_channel_id` = 94 AND `dianxin_channel_id` = 94 WHERE `create_time` >  ".$tody_time); */
             $mysql_connect->table('yx_sfl_multimedia_message')->where('create_time', '>', $tody_time)->update(['free_trial' => 2, 'yidong_channel_id' => 94, 'liantong_channel_id' => 94, 'dianxin_channel_id' => 94]);
