f5f48f71df6853f8ff1540a722512f84a0ee3b7b
Wind-dust <1002380377@qq.com>
2020-06-25 15:22:48 +0800

    短信发送

--- application/console/com/CmppCreateCodeTask.php
+++ application/console/com/CmppCreateCodeTask.php
@@ -8042,13 +8042,13 @@ class CmppCreateCodeTask extends Pzlife
         $tody_time = strtotime(date("Ymd", time()));
         // $tody_time = 1592478000;
         try {
-            $mysql_connect->table('yx_sfl_send_task')->where([['create_time', '>', $tody_time]])->update(['free_trial' => 2, 'yidong_channel_id' => 83, 'liantong_channel_id' => 84, 'dianxin_channel_id' => 84]);
+            // $mysql_connect->table('yx_sfl_send_task')->where([['create_time', '>', $tody_time]])->update(['free_trial' => 2, 'yidong_channel_id' => 83, 'liantong_channel_id' => 84, 'dianxin_channel_id' => 84]);
             /* $where = [];
             $where = [['create_time','>',$tody_time],['template_id', '<>','100150821']];
             $mysql_connect->table('yx_sfl_send_task')->where($where)->update(['free_trial' => 2, 'yidong_channel_id' => 86, 'liantong_channel_id' => 88, 'dianxin_channel_id' => 87]);*/
             // $sendid = $mysql_connect->query("SELECT `id` FROM yx_sfl_send_task WHERE  `template_id` IN ('100181818','100181820') AND `create_time` >  '" . $tody_time ."' ");
             // echo "SELECT `id` FROM yx_sfl_send_task WHERE `template_id` = '100181593' AND `create_time` >  " . $tody_time;die;
-            $sendid = $mysql_connect->query("SELECT `id` FROM `sflsftp`.`yx_sfl_send_task` WHERE `template_id` IN ('100181850','100181855','100181873','100181874','100181874') ");
+            $sendid = $mysql_connect->query("SELECT `id` FROM `sflsftp`.`yx_sfl_send_task` WHERE `template_id` IN ('100181875') ");
             // echo "SELECT `id` FROM yx_sfl_send_task WHERE `template_id` = '100180528' AND `create_time` >  " . $tody_time;die;
             foreach ($sendid as $key => $value) {
                 $this->redis->rpush('index:meassage:sflmessage:sendtask', $value['id']);
@@ -8057,8 +8057,8 @@ class CmppCreateCodeTask extends Pzlife
             exception($th);
         }
         // die;
-        $deduct = 2; //1扣量,2不扣
-        $rate = 50;
+        $deduct = 1; //1扣量,2不扣
+        $rate = 20;
 
         $ids = [];
         $j = 1;
