0b88fcdee7f418516c9e66534b501343e583876e
wujunjie <917436062@qq.com>
2020-07-24 11:13:39 +0800

    ''

--- application/index/controller/Send.php
+++ application/index/controller/Send.php
@@ -1164,10 +1164,9 @@ class Send extends MyController
      * @apiParam (入参) {String} template_id 彩信模板报备的template_id 内容替换为模板中文字内容变量
      * @apiParam (入参) {Array} connect 变量数组
      * @apiParam (connect) {String} mobile 手机号
-     * @apiParam (connect) {String} {{var1}} 变量1内容
-     * @apiParam (connect) {String} {{var2}}
-     * @apiParam (connect) {String} ...
-     * @apiParam (connect) {String} {{var10}} 变量10内容 
+     * @apiParam (connect) {String} var1变量1内容
+     * @apiParam (connect) {String} ar2
+     * @apiParam (connect) {String} var10 变量10内容
      * @apiSuccess (返回) {String} code 200:成功 / 3000:用户名或密码错误 / 3001:手机号格式错误 / 3002:单批次手机号码为空 / 3003:send_time发送时间格式错误 / 3004:预约发送时间小于当前时间 / 3005:该账户没有此项服务 / 3006:余额不足 / 3007:title 短信标题不能为空 / 3008:无效的图片 / 3009:彩信文件长度超过100KB或内容为空 / 3010 图片未上传过 / 3011:服务器错误
      * @apiSampleRequest /index/send/submitBatchCustomMultimediaMessage
      * @author rzc
