dd49264a3d62c27a55219eba61a07c0faef8c826
wind-dust <1002380377@qq.com>
2020-08-27 19:17:39 +0800

    短信发送

--- application/console/com/SflUpload.php
+++ application/console/com/SflUpload.php
@@ -962,7 +962,7 @@ class SflUpload extends Pzlife
                         exception($e);
                     }
                 }
-                $deduct = ceil(800000 / 1120052 * 100);
+                $deduct = ceil(630000 / 888307 * 100);
 
                 /* 扣量 */
                 // $all_num = [0,1,2,3,4];
